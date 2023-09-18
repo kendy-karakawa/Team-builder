9299d2e0901edd73d78e5629ee261b9eb6fb80f9
kendy-karakawa <kendy.karakawa@gmail.com>
2023-09-18 14:35:10 -0300

    test player component

--- font-end/src/app/player/player.component.spec.ts
+++ font-end/src/app/player/player.component.spec.ts
@@ -1,24 +1,54 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { PlayerComponent } from './player.component';
+import { PlayerComponent } from './player.component';
+import { PlayerService } from './player.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { Player } from './player';
 
-// describe('PlayerComponent', () => {
-//   let component: PlayerComponent;
-//   let fixture: ComponentFixture<PlayerComponent>;
+describe('PlayerComponent', () => {
+  let component: PlayerComponent;
+  let fixture: ComponentFixture<PlayerComponent>;
+  let playerService: PlayerService;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [PlayerComponent]
-//     });
-//     fixture = TestBed.createComponent(PlayerComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, FormsModule],
+      declarations: [PlayerComponent],
+      providers: [PlayerService]
+    }).compileComponents();
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    playerService = TestBed.inject(PlayerService);
 
+    fixture = TestBed.createComponent(PlayerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
+  it('should call createPlayer when player name is not null', () => {
+    spyOn(playerService, "createPlayer").and.returnValue(of(null));
+    const fakePlayer: Player = { nome: 'John Doe'};
+    component.player = fakePlayer
+
+    component.postPlayer();
+    expect(playerService.createPlayer).toHaveBeenCalledWith(fakePlayer);
+  });
+
+  it('should alert when player name is null', () => {
+    spyOn(window, 'alert');
+    component.player.nome = null;
+    component.postPlayer();
+    expect(window.alert).toHaveBeenCalledWith('Imput esta vazio');
+  });
+
+  it('should call deleteAll on deleteAll', () => {
+    spyOn(playerService, "deleteAll").and.returnValue(of(null))
+    component.deleteAll();
+    expect(playerService.deleteAll).toHaveBeenCalled();
+  });
+});
