ecd7f4ab70354f50d03775717c2341fddfc5dc28
kendy-karakawa <kendy.karakawa@gmail.com>
2023-09-18 14:50:24 -0300

    test: in app.component

--- font-end/src/app/app.component.spec.ts
+++ font-end/src/app/app.component.spec.ts
@@ -1,29 +1,35 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { AppComponent } from './app.component';
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
-// describe('AppComponent', () => {
-//   beforeEach(() => TestBed.configureTestingModule({
-//     imports: [RouterTestingModule],
-//     declarations: [AppComponent]
-//   }));
+describe('AppComponent', () => {
+  beforeEach((() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [
+        AppComponent,
+        HeaderComponent,
+      ],
+    }).compileComponents();
+  }));
 
-//   it('should create the app', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.componentInstance;
-//     expect(app).toBeTruthy();
-//   });
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
 
-//   it(`should have as title 'font-end'`, () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.componentInstance;
-//     expect(app.title).toEqual('font-end');
-//   });
+  it(`should have as title 'font-end'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('font-end');
+  });
 
-//   it('should render title', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     fixture.detectChanges();
-//     const compiled = fixture.nativeElement as HTMLElement;
-//     expect(compiled.querySelector('.content span')?.textContent).toContain('font-end app is running!');
-//   });
-// });
+  it('should render header', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('app-header')).not.toBe(null);
+  });
+});
