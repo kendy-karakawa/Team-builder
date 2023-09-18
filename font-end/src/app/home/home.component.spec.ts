0daa9d89114f439b7b8f1c3a45af7bcd0a6112f7
kendy-karakawa <kendy.karakawa@gmail.com>
2023-09-18 14:42:59 -0300

    test: in hime.component

--- font-end/src/app/home/home.component.spec.ts
+++ font-end/src/app/home/home.component.spec.ts
@@ -1,22 +1,26 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
 
-// import { HomeComponent } from './home.component';
+import { HomeComponent } from './home.component';
+import { PlayerComponent } from '../player/player.component';
+import { TeamComponent } from '../team/team.component';
 
-// describe('HomeComponent', () => {
-//   let component: HomeComponent;
-//   let fixture: ComponentFixture<HomeComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [HomeComponent],
-//       imports:['app-player', 'app-team']
-//     });
-//     fixture = TestBed.createComponent(HomeComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HomeComponent, PlayerComponent, TeamComponent],
+      imports:[HttpClientTestingModule, FormsModule]
+    });
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
